25556449b75dad7ea3b05c2537223f5c6db97020
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-18 05:03:25 +0200

    Update traverses_linked_list.php add add_node method

--- traverses_linked_list.php
+++ traverses_linked_list.php
@@ -13,4 +13,17 @@ class LinkedList {
     public function __construct() {
         $this->head = null;
     }
+    public function add_node($value) {
+        $new_node = new Node($value);
+        if ($this->head === null) {
+            $this->head = $new_node;
+        } else {
+            $current = $this->head;
+            while ($current->next !== null) {
+                $current = $current->next;
+            }
+            $current->next = $new_node;
+        }
+    }
+
 }
