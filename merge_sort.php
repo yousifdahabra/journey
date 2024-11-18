0d627e88fac2e5a0222e1793b7ea941a43f478d3
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-18 04:40:16 +0200

    Update merge_sort.php function merge handle loop for each array and compare index then sorted it

--- merge_sort.php
+++ merge_sort.php
@@ -3,4 +3,16 @@
 function merge($left, $right){
     $merged = [];
     $i = $j = 0;
+    while ($i < count($left) && $j < count($right)) {
+        if ($left[$i] < $right[$j]) {
+            $merged[] = $left[$i];
+            $i++;
+        } else {
+            $merged[] = $right[$j];
+            $j++;
+        }
+    }
+    $merged = array_merge($merged, array_slice($left, $i));
+    $merged = array_merge($merged, array_slice($right, $j));
+    return $merged;
 }
