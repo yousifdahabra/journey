ad5df5a0adf3ea92a012da4470c22ca3fabec899
YousifDeveloper <yousif.dahabra.97@gmail.com>
2024-11-18 04:43:16 +0200

    Update merge_sort.php add function merge_sort to split and recursive

--- merge_sort.php
+++ merge_sort.php
@@ -1,4 +1,5 @@
 <?php
+//http://localhost/journey/merge_sort.php?list=[5,2,9,1,5,6]
 
 function merge($left, $right){
     $merged = [];
@@ -15,4 +16,16 @@ function merge($left, $right){
     $merged = array_merge($merged, array_slice($left, $i));
     $merged = array_merge($merged, array_slice($right, $j));
     return $merged;
+}
+
+function merge_sort($list){
+    if (count($list) === 1) {
+        return $list;
+    }
+    $mid = intval(count($list) / 2);
+    $left_part = array_slice($list, 0, $mid);
+    $right_part = array_slice($list, $mid);
+    $left_part = merge_sort($left_part);
+    $right_part = merge_sort($right_part);
+    return merge($left_part, $right_part);
 }
